721e83cf9df70318d4467a46a294911a2a49ffe7
Nagagopi <nagagopi@hotmail.com>
2019-05-13 12:08:04 +0530

    sec

--- vars/sammaven.groovy
+++ vars/sammaven.groovy
@@ -1,27 +1,11 @@
 #!/usr/bin/env groovy
 
-pipeline {
-    agent any
-    
-    stages {
-        stage ('Initialize') {
-            steps {
-                sh '''
-                    echo "PATH = ${PATH}"
-                    echo "M2_HOME = ${maven_home}"
-                '''
-            }
-        }
-
-        stage ('Build') {
-            steps {
-                sh 'mvn -Dmaven.test.failure.ignore=true install' 
-            }
-            post {
-                success {
-                    junit 'target/surefire-reports/**/*.xml' 
-                }
-            }
-        }
-    }
-}
+ def call(Map config) {
+      try {
+          sh "mvn clean test -Dmaven.test.failure.ignore=true ${config?.params ?: ''}"
+      } finally {
+          def path = config?.artifactsPath?.concat('/') ?: ''
+          junit testResults: "${path}target/*.xml", allowEmptyResults: true
+          archiveArtifacts "${path}target/**"
+      }
+  }
