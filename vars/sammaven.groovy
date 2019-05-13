547bfa6582ad9d3a07e597db5a43fab0c8d21c89
Nagagopi <nagagopi@hotmail.com>
2019-05-13 11:55:25 +0530

    second

--- vars/sammaven.groovy
+++ vars/sammaven.groovy
@@ -1,5 +1,27 @@
 #!/usr/bin/env groovy
 
-def call(String name = 'human') {
-  echo "Hello, ${name}."
+pipeline {
+    agent any
+    
+    stages {
+        stage ('Initialize') {
+            steps {
+                sh '''
+                    echo "PATH = ${PATH}"
+                    echo "M2_HOME = ${maven_home}"
+                '''
+            }
+        }
+
+        stage ('Build') {
+            steps {
+                sh 'mvn -Dmaven.test.failure.ignore=true install' 
+            }
+            post {
+                success {
+                    junit 'target/surefire-reports/**/*.xml' 
+                }
+            }
+        }
+    }
 }
