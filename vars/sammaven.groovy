df117ffedd9e4f4cbf50adfbd667ca71cd7802d1
Nagagopi <nagagopi@hotmail.com>
2019-05-13 12:43:24 +0530

    tsetht

--- vars/sammaven.groovy
+++ vars/sammaven.groovy
@@ -2,7 +2,7 @@
 
  def call(Map config) {
       try {
-          sh "mvn clean test -Dmaven.test.failure.ignore=true ${config?.params ?: ''}"
+          sh "mvn package test -Dmaven.test.failure.ignore=true ${config?.params ?: ''}"
       } finally {
           def path = config?.artifactsPath?.concat('/') ?: ''
           junit testResults: "${path}target/*.xml", allowEmptyResults: true
