bf0094b037f0773e1272baa22279ebd64a6fe2f1
keerthiuppala <50653611+keerthiuppala@users.noreply.github.com>
2019-05-15 19:23:30 +0530

    efrg

--- vars/MainFile.groovy
+++ vars/MainFile.groovy
@@ -6,8 +6,10 @@ def call()
 	node {
 		echo "11111111111111"
 		def filename = 'env.yml'
-  def datas = readYaml file: filename
+		println filename.txt
+  		def datas = readYaml file: filename
 		echo datas.branch.toString()
+		echo "222222222222222"
 }
 pipeline {
     agent any
