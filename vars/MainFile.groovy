8721d9b03a183fcc95bd9685cd08dffa6fa6abb1
keerthiuppala <50653611+keerthiuppala@users.noreply.github.com>
2019-05-15 11:57:03 -0400

    reqref

--- vars/MainFile.groovy
+++ vars/MainFile.groovy
@@ -11,17 +11,21 @@ def call()
     stages {
 	    
 	    
-	    
+	    echo "11111111111111111111111111111111"
 	    
 	    stage('Read YAML file') {
         steps {
+		echo "222222222222222222222222222222222222"
             script{ datas = readYaml (file: 'env.yml') }
+		echo "333333333333333333333333333333333333"
                   }
     }
 	    
 	    
 	    
 	    stage('Checkout') {
+		    
+		    echo "4444444444444444444444444444444444444444444"
 			steps {
 				scmFile(datas.branch,datas.gitUrl)
 			}
