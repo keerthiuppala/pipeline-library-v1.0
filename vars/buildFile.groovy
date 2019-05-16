f0e84e11bd4b7ba51ab8069e30ef6f31caaa43fa
keerthiuppala <50653611+keerthiuppala@users.noreply.github.com>
2019-05-16 14:33:13 +0530

    hmj

--- vars/buildFile.groovy
+++ vars/buildFile.groovy
@@ -1,7 +1,6 @@
 #!/usr/bin/env groovy
 
 def call(def datas) {
-	echo datas.buildTool.toString()
 		def mvnHome = tool "${datas.buildTool}";
 	sh "${mvnHome}/bin/mvn "+"${datas.mavenGoals}"+" -Dbuild.number=${BUILD_NUMBER}"
 }
