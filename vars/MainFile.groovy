fbae96bfd87a661c579af344eae9ea2314bb39c2
keerthiuppala <50653611+keerthiuppala@users.noreply.github.com>
2019-05-16 14:34:24 +0530

    gth

--- vars/MainFile.groovy
+++ vars/MainFile.groovy
@@ -2,53 +2,40 @@
 
 def call()
 {
-	pipeline {
+pipeline {
     agent any
-	
 	options {
     skipDefaultCheckout(true)
-	}
+		}
     stages {
-    
-	    
-	    
-	    
-	    stage('Checkout') {
-		    
-		    
+    	    stage('Checkout') {    
 			steps {
 				scmFile("master","https://github.com/keerthiuppala/maven-project.git")
 			}
 		}
 	    stage('Read YAML file') {
-        steps {
-		echo "222222222222222222222222222222222222"
-            script{ datas = readYaml (file: 'env.yml') }
-		echo datas.branch.toString()
-		echo "333333333333333333333333333333333333"
-                  }
-    }
+        		steps {
+            			script{ datas = readYaml (file: 'env.yml') }
+                  	}
+    		}
 	    stage('Build') {
 			steps {
-				echo "4444444444444444444444444444444444"
-				echo datas.buildTool.toString()
 				buildFile(datas)
-				echo "5555555555555555555555555555555555555"
 			}
 		}
 	    stage('Upload Artifacts') {
 			steps {
-				uploadArtifactory(datas.artifactoryTool)
+				uploadArtifactory(datas)
 			}
 		}
 	    stage('Download Artifacts') {
 			steps {
-				downloadArtifactory(datas.artifactoryTool)
+				downloadArtifactory(datas)
 			}
 		}
 	    stage('Publish Junit Reports') {
 			steps {
-				junitFile()
+				junitFile(datas)
 			}
 		}
 
